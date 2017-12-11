89067e60e34d48e044517a3cde42e47254ecb77c
Anderson, Daniel <andersond@uncw.edu>
2017-12-11 16:53:17 -0500

    Add initial database migration

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -3,16 +3,16 @@ using SamuraiApp.Domain;
 
 namespace SamuraiApp.Data
 {
-    public class SamuraiContext : DbContext
-    {
-        public DbSet<Samurai> Samurais { get; set; }
-        public DbSet<Battle> Battles { get; set; }
-        public DbSet<Quote> Quotes { get; set; }
+  public class SamuraiContext : DbContext
+  {
+    public DbSet<Samurai> Samurais { get; set; }
+    public DbSet<Battle> Battles { get; set; }
+    public DbSet<Quote> Quotes { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(
-              "Server = (localdb)\\mssqllocaldb; Database = SamuraiData; Trusted_Connection = True; ");
-        }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseSqlServer(
+        "Server = (localdb)\\mssqllocaldb; Database = SamuraiData; Trusted_Connection = True; ");
     }
+  }
 }
--- SamuraiApp.Domain/Samurai.cs
+++ SamuraiApp.Domain/Samurai.cs
@@ -13,5 +13,5 @@ namespace SamuraiApp.Domain
     public string Name { get; set; }
     public List<Quote> Quotes { get; set; }
     public int BattleId { get; set; }
-    }
+  }
 }
