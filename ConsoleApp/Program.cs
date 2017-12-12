ed6c89814d92e9a5fbded5acf73b6c4b801943b1
Anderson, Daniel <andersond@uncw.edu>
2017-12-12 16:52:29 -0500

    Add Console project and demonstrate basic Insertion

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Logging;
+using SamuraiApp.Data;
+using SamuraiApp.Domain;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            InsertSamurai();
+        }
+
+        private static void InsertSamurai()
+        {
+            var samurai = new Samurai { Name = "Test" };
+            using (var context = new SamuraiContext())
+            {
+                //context.GetService<ILoggerFactory>().AddProvider(new MyLoggerProvider());
+                context.Samurais.Add(samurai);
+                context.SaveChanges();
+            }
+        }
+    }
+}
