7f88efc9df7f55e453829b0b45cb33bcdfd7ed29
Anderson, Daniel <andersond@uncw.edu>
2017-12-12 14:36:10 -0500

    Add Join Entity to relevant domain models and configure accordingly

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -3,16 +3,23 @@ using SamuraiApp.Domain;
 
 namespace SamuraiApp.Data
 {
-  public class SamuraiContext : DbContext
-  {
-    public DbSet<Samurai> Samurais { get; set; }
-    public DbSet<Battle> Battles { get; set; }
-    public DbSet<Quote> Quotes { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    public class SamuraiContext : DbContext
     {
-      optionsBuilder.UseSqlServer(
-        "Server = (localdb)\\mssqllocaldb; Database = SamuraiData; Trusted_Connection = True; ");
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>()
+                    .HasKey(s => new { s.BattleId, s.SamuraiId });
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+              "Server = (localdb)\\mssqllocaldb; Database = SamuraiData; Trusted_Connection = True; ");
+        }
     }
-  }
 }
